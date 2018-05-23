9075370d48b6fc808612bf8c010d5ecb9d976162
Thilo <thilo.segschneider@uni-ulm.de>
2018-05-23 20:16:10 +0200

    Erster Versuch

--- src/lmao/Main.java
+++ src/lmao/Main.java
@@ -4,8 +4,11 @@ import com.leapmotion.leap.Controller;
 import com.leapmotion.leap.Listener;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,11 +17,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        // fx shit
+        Parent root = FXMLLoader.load(getClass().getResource("Menue.fxml"));
         primaryStage.setTitle("LMAO");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root));
+
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+        // fx shit
+        primaryStage.setMaximized(true);
+
         primaryStage.show();
 
     }
--- src/lmao/javafxController.java
+++ src/lmao/javafxController.java
@@ -1,14 +1,33 @@
 package lmao;
 
-import com.leapmotion.leap.Controller;
-import com.leapmotion.leap.Listener;
+import com.leapmotion.leap.*;
 import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Label;
+import javafx.scene.media.AudioClip;
+import javafx.stage.Screen;
+
+import javax.print.DocFlavor;
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class javafxController {
 
     //fxml
-    public Label label;
+   // public Label label;
+
+    double x;
+    double y;
+    int appWidth;
+    int appHeight;
+
+    Robot robot = new Robot();
+
+    AudioClip plonkSound = new AudioClip(Paths.get("src/lmao/bomb.mp3").toUri().toString());
 
     //setting up controller
     Controller controller = new Controller();
@@ -16,21 +35,57 @@ public class javafxController {
     AnimationTimer animationTimer = new AnimationTimer() {
         @Override
         public void handle(long now) {
-            label.setText(controller.frame().hands().get(0).palmPosition().toString());
+//            label.setText(controller.frame().hands().get(0).fingers().get(0).stabilizedTipPosition().toString());
+            x = controller.frame().hands().get(0).fingers().get(0).stabilizedTipPosition().getX();
+            y = controller.frame().hands().get(0).fingers().get(0).stabilizedTipPosition().getY();
+
+
+            InteractionBox iBox = controller.frame().interactionBox();
+            Pointable pointable = controller.frame().pointables().frontmost();
+
+            Vector leapPoint = pointable.stabilizedTipPosition();
+            Vector normalizedPoint = iBox.normalizePoint(leapPoint, true);
+
+            float appX = normalizedPoint.getX() * appWidth;
+            float appY = (1 - normalizedPoint.getY()) * appHeight;
+
+            robot.mouseMove((int) appX, (int) appY);
+
+            for(Gesture g:controller.frame().gestures()){
+                if(g.type() == Gesture.Type.TYPE_SCREEN_TAP){
+                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                }
+            }
+
+            if(appHeight - appY < 100){
+                plonkSound.play();
+            }
         }
     };
+
+    public javafxController() throws AWTException {
+    }
 
 
     public void initialize(){
 
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        appWidth = (int) bounds.getWidth();
+        appHeight = (int) bounds.getHeight();
         //init controller
         controller.addListener(listener);
         animationTimer.start();
 
+        controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+
     }
 
 
+    public void start(ActionEvent actionEvent) {
+    }
 
-
-
+    public void exit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
