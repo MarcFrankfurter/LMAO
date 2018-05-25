4f821190d538fb4256e38afec9b10bb460884f9f
marc <marc.frankfurter@uni-ulm.de>
2018-05-25 09:52:06 +0200

    nur de baba darf busche

--- src/lmao/javafxController.java
+++ src/lmao/javafxController.java
@@ -3,7 +3,9 @@ package lmao;
 import com.leapmotion.leap.*;
 import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.media.AudioClip;
 import javafx.stage.Screen;
@@ -20,6 +22,26 @@ import java.nio.file.Paths;
 public class javafxController {
 
     //fxml
+    @FXML
+    Button line1;
+
+    @FXML
+    Button line2;
+
+    @FXML
+    Button line3;
+
+    @FXML
+    Button line4;
+
+    @FXML
+    Button line5;
+
+    @FXML
+    Button line6;
+
+    @FXML
+    Button line7;
     // public Label label;
 
     double x;
@@ -27,6 +49,16 @@ public class javafxController {
     int appWidth;
     int appHeight;
     boolean B = false;
+
+    InteractionBox iBox;
+    Pointable pointable;
+    Vector leapPoint;
+    Vector normalizedPoint;
+
+    float appX;
+    float appY;
+
+    int eighth;
 
     Robot robot = new Robot();
 
@@ -42,14 +74,14 @@ public class javafxController {
             y = controller.frame().hands().get(0).fingers().get(0).stabilizedTipPosition().getY();
 
 
-            InteractionBox iBox = controller.frame().interactionBox();
-            Pointable pointable = controller.frame().pointables().frontmost();
+            iBox = controller.frame().interactionBox();
+            pointable = controller.frame().pointables().frontmost();
 
-            Vector leapPoint = pointable.stabilizedTipPosition();
-            Vector normalizedPoint = iBox.normalizePoint(leapPoint, true);
+            leapPoint = pointable.stabilizedTipPosition();
+            normalizedPoint = iBox.normalizePoint(leapPoint, true);
 
-            float appX = normalizedPoint.getX() * appWidth;
-            float appY = (1 - normalizedPoint.getY()) * appHeight;
+            appX = normalizedPoint.getX() * appWidth;
+            appY = (1 - normalizedPoint.getY()) * appHeight;
 
             robot.mouseMove((int) appX, (int) appY);
 
@@ -64,7 +96,23 @@ public class javafxController {
 
                 //plonkSound.play();
                 if (!B) {
-                    Play.midi(new Note());
+                    if (appX < eighth) {
+                        Play.midi(new Note(60, 1));
+                    } else if (appX >= eighth && appX < 2 * eighth) {
+                        Play.midi(new Note(62, 1));
+                    } else if (appX >= 2 * eighth && appX < 3 * eighth) {
+                        Play.midi(new Note(64, 1));
+                    } else if (appX >= 3 * eighth && appX < 4 * eighth) {
+                        Play.midi(new Note(65, 1));
+                    } else if (appX >= 4 * eighth && appX < 5 * eighth) {
+                        Play.midi(new Note(67, 1));
+                    } else if (appX >= 5 * eighth && appX < 6 * eighth) {
+                        Play.midi(new Note(69, 1));
+                    } else if (appX >= 6 * eighth && appX < 7 * eighth) {
+                        Play.midi(new Note(71, 1));
+                    } else if (appX >= 7 * eighth && appX < appWidth) {
+                        Play.midi(new Note(72, 1));
+                    }
                     B = true;
                 }
             } else {
@@ -89,10 +137,15 @@ public class javafxController {
 
         controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
 
-    }
+        eighth = appWidth/8;
+        line1.setLayoutX(eighth);
+        line2.setLayoutX(2 * eighth);
+        line3.setLayoutX(3 * eighth);
+        line4.setLayoutX(4 * eighth);
+        line5.setLayoutX(5 * eighth);
+        line6.setLayoutX(6 * eighth);
+        line7.setLayoutX(7 * eighth);
 
-
-    public void start(ActionEvent actionEvent) {
     }
 
     public void exit(ActionEvent actionEvent) {
